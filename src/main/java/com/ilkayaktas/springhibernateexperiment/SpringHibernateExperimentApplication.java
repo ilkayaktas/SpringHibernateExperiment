eb173f859d8c40337ba75ed4333d3c471404b213
İlkay AKTAŞ <iaktas@aselsan.com.tr>
2022-01-29 14:19:01 +0300

    Spring JPA Project is initialized with liquebase library.

---
+++ src/main/java/com/ilkayaktas/springhibernateexperiment/SpringHibernateExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.ilkayaktas.springhibernateexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateExperimentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilkayaktas/springhibernateexperiment/SpringHibernateExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkayaktas.springhibernateexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
